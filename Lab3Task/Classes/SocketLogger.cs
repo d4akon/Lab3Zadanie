654fc39ebb68baa74ed746e340197238c40c5dfb
Szymon Maj <szymon.maj@icloud.com>
2022-04-22 21:08:30 +0200

    Added ClientSocket, SocketLogger and WriterLogger classes

---
+++ Lab3Task/Classes/SocketLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Lab3Task.Interfaces;
+
+namespace Lab3Task.Classes
+{
+    class SocketLogger : ClientSocket, ILogger
+    {
+        private ClientSocket clientSocket;
+
+        public SocketLogger(string host, int port)
+        {
+
+        }
+
+        ~SocketLogger()
+        {
+            this.Dispose(false);
+        }
+
+        public void Log (params ) 
+
+        public void Dispose()
+        {
+            this.Dispose(disposing: true);
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Lab3Task/Classes/WriterLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab3Task.Interfaces;
+
+namespace Lab3Task.Classes
+{
+    public abstract class WriterLogger : ILogger
+    {
+        protected TextWriter writer;
+
+        public virtual void Log(params string[] message)
+        {
+            //TODO
+            // Uzupełnić to miejsce o logikę zapisu opartą o TextWriter ...
+        }
+
+        public abstract void Dispose();
+    }
+}
