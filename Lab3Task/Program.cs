88eeb7975184ac5be449a623d9293806125e2c37
Szymon Maj <szymon.maj@icloud.com>
2022-04-22 20:05:16 +0200

    Implemented Program Class

--- Lab3Task/Program.cs
+++ Lab3Task/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using ConsoleApp.Logger;
 
 namespace Lab3Task
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            ILogger[] loggers = new ILogger[]
+            {
+                new ConsoleLogger(),
+                new FileLogger("log.txt"),
+                new SocketLogger("google.com", 80)
+            };
+
+            using (ILogger logger = new CommonLogger(loggers))
+            {
+                logger.Log("Example message 1 ...");
+                logger.Log("Example message 2 ...");
+                logger.Log("Example message 3 ...", "value 1", "value 2", "value 3");
+            }
         }
     }
 }
